79295852a099c56cd36aab2ee50589a2da841039
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-03-02 19:29:05 +0100

    Création d'un type (sous la forme d'une classe), modélisant une mesure, du benchmark

---
+++ src/main/java/ch/heigvd/res/io/util/Metric.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.io.util;
+
+import ch.heigvd.res.io.BufferedIOBenchmark.IOStrategy;
+
+/**
+ *
+ * @author Fabien Franchini
+ */
+public class Metric {
+
+    public Metric(String operation, IOStrategy strategy, int blockSize, int fileSize, int duration) {
+        this.operation = operation;
+        this.strategy = strategy;
+        this.blockSize = blockSize;
+        this.fileSize = fileSize;
+        this.duration = duration;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public IOStrategy getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(IOStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public int getBlockSize() {
+        return blockSize;
+    }
+
+    public void setBlockSize(int blockSize) {
+        this.blockSize = blockSize;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    private String operation;
+    private IOStrategy strategy;
+    private int blockSize;
+    private int fileSize;
+    private int duration;
+}
+    
