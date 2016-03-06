e9b1c0fbdc964b3e5e091e0c49fb5da85799c36a
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-03-06 18:14:12 +0100

    Définition / Implementation du type BufferedIOBenchmarkData, implémentant l'interface IData

--- src/main/java/ch/heigvd/res/io/util/BufferedIOBenchmarkData.java
+++ src/main/java/ch/heigvd/res/io/util/BufferedIOBenchmarkData.java
@@ -5,10 +5,32 @@
  */
 package ch.heigvd.res.io.util;
 
+import ch.heigvd.res.io.BufferedIOBenchmark.IOStrategy;
+import java.util.Collection;
+import java.util.Map;
+
 /**
  *
  * @author crab_one
  */
 public class BufferedIOBenchmarkData implements IData {
+    
+    private Map<String, Object> values;
+
+    public BufferedIOBenchmarkData(String operation, IOStrategy strategy, int blockSize, int fileSize, int duration) {
+        values.put("operation", operation);
+        values.put("strategy", strategy);
+        values.put("blockSizeInBytes", blockSize);
+        values.put("fileSizeInBytes", fileSize);
+        values.put("duration", duration);
+    }
+    
+    public Collection<String> getKeys() {
+        return values.keySet(); 
+    }
+    
+    public Collection<Object> getValues() {
+        return values.values();
+    }
     
 }
