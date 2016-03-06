99946f1c3dc51da9cd5f7bd2033ffdb514583db2
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-03-06 18:42:21 +0100

    Définition de la classe FileRecorder implémentant l'interface IRecorder

--- src/main/java/ch/heigvd/res/io/util/FileRecorder.java
+++ src/main/java/ch/heigvd/res/io/util/FileRecorder.java
@@ -11,4 +11,15 @@ package ch.heigvd.res.io.util;
  */
 public class FileRecorder implements IRecorder {
     
+    private String fileName;
+    private ISerializer serializer;
+    
+    public FileRecorder(String fileName, ISerializer serializer) {
+        this.fileName = fileName;
+        this.serializer = serializer;        
+    }
+    
+    public void record(IData data) {
+        // TODO...
+    }
 }
